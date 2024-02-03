91ffa8c817dbabfdfbf6a49740b53f20c6c5550b
みのむし <mm1640052@gmail.com>
2024-02-03 16:00:00 +0900

    station4 clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 // DO NOT DELETE
 
 import './App.css'
+import React, { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
   return (
     <header>
       Dogアプリ<p>犬の画像を表示するサイトです</p>
-      <img src="./src/dog.jpg" />
+      {/* <img src="./src/dog.jpg"   /> */}
+      <img src={dogUrl} />
     </header>
   )
 }
